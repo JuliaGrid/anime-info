81cd9b9d04e52006c9e8350bbf6009fc7c9f9c7e
Юлия Герасименко <Julia.grid2000@gmail.com>
2025-07-12 12:36:16 +0300

    init: react app

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="wrapper">
+      <h1>Star wars</h1>
+      <div className="controls">
+        <input />
+        <button>Search</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
